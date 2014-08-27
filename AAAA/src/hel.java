99722970335829c8cdfbadc486b90fbc373ec4ac
zhufubo <1653312607@qq.com>
2014-08-27 16:09:05 +0800

    1

--- AAAA/src/hel.java
+++ AAAA/src/hel.java
@@ -3,5 +3,6 @@ public class hel {
      public void main(String args[]){
     	 System.out.println("baidu.com");
     	 System.out.println("shouhu.com");
+    	 System.out.println("YES,NO");
      }
 }
